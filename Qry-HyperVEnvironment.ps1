b2c44c2e0cafa1fb87a56247b1075ebd732450d5
FsckMyLife <alex.tester@observicing.net>
2017-11-28 12:30:42 -0500

    Adding a few items

---
+++ Qry-HyperVEnvironment.ps1
@@ -0,0 +1,37 @@
+﻿#Works for 2012R2
+#Need to document requires amongst other things
+
+$Results=@()
+$Results2=@()
+$QryHVServers=Get-ADObject -Filter 'ObjectClass -eq "serviceConnectionPoint" -and Name -eq "Microsoft Hyper-V"'
+$Hypervisors= @($QryHVServers | Select-Object -Property @{label='DistinguishedName';expression={$_.distinguishedname -replace '^CN=Microsoft Hyper-V,CN=|,.*$'}})
+$QryHVVMs=Get-ADObject -Filter 'ObjectClass -eq "serviceConnectionPoint" -and Name -eq "Windows Virtual Machine"'
+$HVVMs= @($QryHVVMs | Select-Object -Property @{label='DistinguishedName';expression={$_.distinguishedname -replace '^CN=Windows Virtual Machine,CN=|,.*$'}})
+foreach ($HyperVisor in $Hypervisors) {
+$SvrObj=""
+$SvrObj=Get-ADComputer $HyperVisor.distinguishedname
+    $Dtl=""
+            $Dtl = @{            
+            Server  = $SvrObj.name
+            DN        = $SvrObj.distinguishedname
+            Role = "Hyper-V Host"
+            }
+
+     $Results+=New-Object PSObject -Property $Dtl 
+
+}
+
+foreach ($HVVM in $HVVMS) {
+$SvrObj=""
+$SvrObj=Get-ADComputer $HVVM.distinguishedname
+    $Dtl2=""
+            $Dtl2 = @{            
+            Server  = $SvrObj.name
+            DN        = $SvrObj.distinguishedname
+            Role = "Hyper-V Guest"
+            }
+
+     $Results+=New-Object PSObject -Property $Dtl2
+
+}
+$Results #Here you go!
---
+++ Qry-iLOPowerConsumption/Qry-iLOPowerConsumption.ps1
@@ -0,0 +1,24 @@
+﻿# This is quick and dirty way to log HP power consumption via iLO3 over a short period of time
+     $Cred=Get-Credential
+     $Server=Read-Host "Enter iLO server name"
+     $Results=@()
+     #Lazy loop
+     $meow="1"
+     while ($meow -eq "1"){
+        $TmpDtl=""
+        $Query=""
+        $DT=""
+        $Dt=Get-Date
+        $Query=Get-HPiLOPowerReading -Server $Server -Credential $cred -DisableCertificateAuthentication
+            $TmpDtl = @{            
+            Avg = $Query.AVERAGE_POWER_READING
+            Max = $Query.MAXIMUM_POWER_READING
+            Min = $Query.MINIMUM_POWER_READING
+            Cur = $Query.PRESENT_POWER_READING
+            Dt  = $dt.DateTime
+        }
+     $Results+=New-Object PSObject -Property $TmpDtl 
+    #save, wait, do it again
+    $Results | ConvertTo-Html | Out-File C:\Scripts\iLOQueryResults.html -Append
+    sleep 600
+}
